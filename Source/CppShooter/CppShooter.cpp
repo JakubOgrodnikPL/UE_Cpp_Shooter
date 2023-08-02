5c7f594df4bfb1c52b45c75a9a975ff757fdbcc6
Jakub Ogrodnik <jakub.ogrodnik.pl@gmail.com>
2023-08-02 09:00:58 +0200

    Deleted content, added source folder witch c++ code only due to lack of space

---
+++ Source/CppShooter/CppShooter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CppShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CppShooter, "CppShooter" );
---
+++ Source/CppShooter/CppShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CppShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPSHOOTER_API ACppShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
