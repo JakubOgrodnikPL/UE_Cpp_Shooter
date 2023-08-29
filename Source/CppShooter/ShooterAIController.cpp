ea3235c30c8f50c2ea0becf1a79cfa4674a16937
Jakub Ogrodnik <jakub.ogrodnik.pl@gmail.com>
2023-08-29 06:49:47 +0200

    +PatrollingRoute

--- Source/CppShooter/ShooterAIController.cpp
+++ Source/CppShooter/ShooterAIController.cpp
@@ -9,15 +9,15 @@ void AShooterAIController::BeginPlay()
 {
     Super::BeginPlay();
 
-    APawn* PlayerPawn  = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-
     //SetFocus(PlayerPawn);
 
     if(AIBehavior != nullptr)
     {
         RunBehaviorTree(AIBehavior);
+        APawn* PlayerPawn  = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 
         GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+        GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
     }
 
 }
